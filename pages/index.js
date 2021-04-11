33048abb9810ed9fa2022d985ce045f7beb43f4e
Mateus Marinho <40375289+notmarinho@users.noreply.github.com>
2021-04-11 17:23:05 -0300

    Nova home

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,7 @@
 import { useState } from 'react'
 
 const Home = () => {
-    return (
-        <div>
-            <h2>Home 3</h2>
-            <Contador />
-        </div>
-    )
-}
-
-const Contador = () => {
-    const [contador, setContador] = useState(0)
-
-    function adicionarContador() {
-        setContador(previewState => previewState + 1)
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+    return <h1>Home</h1>
 }
 
 export default Home;
